a63145d7867b11fc2ca94fa910587253194a6500
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2018-07-29 12:17:57 +0100

    home1.py

---
+++ home1.py
@@ -0,0 +1,58 @@
+#Libraries
+#--------------------------------
+import time
+from time import strftime
+import os
+from sense_hat import SenseHat
+import csv
+#--------------------------------
+
+
+#Variables
+#--------------------------------
+count = 0
+now = strftime("%d-%m-%y")
+dir = "/home/pi/WeatherData/"
+path = dir + now
+#--------------------------------
+
+
+#SenseHat Sensors
+#--------------------------------
+def sensors():
+  sense = SenseHat()
+  temp = str(round(sense.get_temperature()))
+  humidity = str(round(sense.get_humidity()))
+  pressure = str(round(sense.get_pressure()))
+  global temp
+  global humidity
+  global pressure
+#--------------------------------
+  
+  
+#Transmission
+#--------------------------------
+#--------------------------------
+  
+  
+#Main
+#--------------------------------
+def main():
+  sensors()
+  global count
+  fields = [temp,pressure,humidity]
+  while True:
+    if count == 2:
+      with open(path, 'ab') as data:
+        writer = csv.writer(data)
+        writer.writerow(fields)
+        data.close()
+        count = 0
+        break
+    else:
+      time.sleep(1)
+      count = count + 1
+      print(count)
+#--------------------------------
+
+main()

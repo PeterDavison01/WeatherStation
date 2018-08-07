7ea5a000abcbb3e37b42dd797833c5510af1df43
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2018-08-07 20:45:30 +0100

    Update home1.py

--- home1.py
+++ home1.py
@@ -76,11 +76,11 @@ def Transmission():
     #distutils.dir_util.copy_tree(LocalDir, NASdir)
     shutil.copytree(Local, NASdir)
     print("Transmitted")
-    except shutil.Error as e:
-        print('Directory not copied. Error: %s' % e)
+  except shutil.Error as e:
+    print('Directory not copied. Error: %s' % e)
     # Any error saying that the directory doesn't exist
-    except OSError as e:
-        print('Directory not copied. Error: %s' % e)
+  except OSError as e:
+    print('Directory not copied. Error: %s' % e)
  
 #--------------------------------
 

d4f2f224c8c8c3ea1cb150847d626c674bc3c559
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2018-08-07 17:30:07 +0100

    Update home1.py

--- home1.py
+++ home1.py
@@ -40,7 +40,7 @@ def main():
   emptycell = ""
   fields = [strftime("%H:%M:%S"),emptycell,temp,pressure,humidity]
   while True:
-    if count == 5:
+    if count == 2:
       with open(LocalPath, 'a') as data:
         writer = csv.writer(data)
         writer.writerow(fields)

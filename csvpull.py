26cc7b78d3d3c851d78a5ea773bb9bcbd28833e5
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2019-01-02 12:30:36 +0000

    Update csvpull.py

--- csvpull.py
+++ csvpull.py
@@ -3,7 +3,7 @@ from time import strftime
 
 now = strftime("%d-%m-%y")
 dir = "/home/pi/WeatherData/"
-path = dir + now + ".csv"
+path = "Timble.csv"
 
 with open(path, 'r') as csv:
   for row in csv:

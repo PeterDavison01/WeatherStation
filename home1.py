8a37b572d556332171d71ed1cef15471cf70942f
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2019-01-02 12:58:29 +0000

    Update home1.py

--- home1.py
+++ home1.py
@@ -61,7 +61,7 @@ def main():
   sensors()
   fields = [strftime("%y-%m-%d %H:%M:%S"),temp,pressure,humidity]
   while True:
-    if strftime("%M:%S") == "55:00":
+    if strftime("%M:%S") == "59:00":
       with open(TestDIR, 'a') as data:
         writer = csv.writer(data)
         writer.writerow(fields)

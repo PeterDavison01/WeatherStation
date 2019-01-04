6f96d0fac8b918d83dc41d7755fbd78349b20f63
PeterDavison01 <35691638+PeterDavison01@users.noreply.github.com>
2019-01-04 15:56:15 +0000

    Update home1.py

--- home1.py
+++ home1.py
@@ -76,6 +76,7 @@ def main():
   sensors()
   print("&&")
   Calcs()
+  print("^^")
   fields = [strftime("%y-%m-%d %H:%M:%S"),temp,pressure,humidity,d_temp,d_pressire,d_humidity]
   while True:
     if strftime("%M:%S") == "54:15":
@@ -102,6 +103,7 @@ def init():
       Transmission()
     except:
       print("Cannot connect ")
+      time.sleep(1)
       continue
 #--------------------------------
 

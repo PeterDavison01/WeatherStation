9c916ea685b1cb936cb0ad4f9fb8f3117e8aa4d9
MPD-01 <47279139+MPD-01@users.noreply.github.com>
2019-02-13 11:00:10 +0000

    Add files via upload

--- weather.py
+++ weather.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
 '''
 from werkzeug.debug import DebuggedApplication
 from myapp import app
@@ -11,27 +13,30 @@ import pickle
 from sklearn.linear_model import LinearRegression
 from time import strftime
 import time
-from io import StringIO
+import io
+# from io import StringIO
 
 app = Flask(__name__)
-# NasDIR = '//192.168.16.20/WeatherStation/Timble Data.csv'
-NasDIR = '//mpd-ds/WeatherStation/Timble Data.csv'
-
+NasDIR = '/mnt/Nas/Timble Data.csv'
+# NasDIR = '//mpd-ds/WeatherStation/Timble Data.csv'
 
 @app.route("/")
 def homepage():
         # read the log file from the NAS
         outputhtml=""
         TD = strftime("%H:%M:%S on %d-%m-%y")
-        with open(NasDIR,'r',encoding='ascii') as NAS:
-                Naslines = NAS.readlines()
-                NAS.close()
+        # datacsv=io.open(NasDIR,'rb')
+
+        with open(NasDIR,'rb') as NAS:
+               Naslines = NAS.readlines()
+               NAS.close()
         # deconstruct last line into useful components
         logdate=np.genfromtxt(Naslines[-1:],delimiter=',',usecols=0,dtype=str)
         logdata=np.genfromtxt(Naslines[-1:],delimiter=',',usecols=(1,2,3,4,5,6),dtype=float)
         currstats=logdata.reshape(1,6)
+
         # load the model, and use it to predict the future temp
-        loaded_model = pickle.load(open('Harrogate Model.sav', 'rb'))
+        loaded_model = pickle.load(open('HarrogateModel.sav', 'rb'))
         result = loaded_model.predict(currstats)
         result = "{:3.1f}".format(result[0])
         # construct the HTML output from all the data gathered
@@ -40,10 +45,9 @@ def homepage():
         outputhtml = outputhtml + "<table border=""1"">"
         outputhtml = outputhtml + "<tr><th>Temperature</p>(°C)</th><th>Pressure</p>(mb)</th><th>Humidity</p>(??)</th><th>Change in</p>Temp (°C)</th><th>Change in</p>Press (mb)</th><th>Change in</p>Humidity (??)</th></tr>"
         outputhtml = outputhtml + "<tr><th>"+str(logdata[0])+"</th><th>"+str(logdata[1])+"</th><th>"+str(logdata[2])+"</th><th>"+str(logdata[3])+"</th><th>"+str(logdata[4])+"</th><th>"+str(logdata[5])+"</th></tr>"
-        outputhtml = outputhtml + "</table>"        
+        outputhtml = outputhtml + "</table>"
         outputhtml = outputhtml + "<h1>Using this data in the model - gives a predicted temperature for the next hour of " + result + "°C</h1>"
         return(outputhtml)
 
 if __name__ == '__main__':
-	app.run(debug=True)
-	
+	app.run(debug=True,port=80,host='0.0.0.0')
